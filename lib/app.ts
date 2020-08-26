da8aee57c9ac8a34b8d6a397d0ac3a985a64fe10
João Gabriel de Medeiros Pereira <gah.mp1@gmail.com>
2020-08-26 20:41:21 -0300

    adjust database

--- lib/app.ts
+++ lib/app.ts
@@ -32,7 +32,7 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
-    console.log("DATABASE:" + DATABASE)
+    console.log("DATABASE:" + this.database())
     console.log("DEVELOPMENT:" + DEVELOPMENT)
     console.log("URL:" + this.mongoUrl)
   }
